2d2885b85534f69887146a766938bfe2c339fed6
Annu Gupta <annugupta.nugen@gmail.com>
2024-11-15 11:37:59 +0530

    feat/design-sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import SidebarComponent from "@/components/sidebarComponent";
+import TopbarComponent from "@/components/topbarComponent";
+
 export default function Home() {
-  return <>hiii Helooo</>;
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full z-20 bg-white">
+        <TopbarComponent />
+      </div>
+      <div className="flex absolute top-[12%]">
+        <div className="h-screen">
+          <SidebarComponent icon={""} label={""} />
+        </div>
+        <div className="flex-grow h-screen ml-4 w-screen bg-gray-200 rounded-md">
+          hello
+        </div>
+      </div>
+    </>
+  );
 }
