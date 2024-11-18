e61a65f3c3ba5aaead76015f6bbf4c91468e65ee
Pardeep24k <pardeep1209k@gmail.com>
2024-11-18 11:45:07 +0530

    top bar

--- src/components/topbarComponent/index.tsx
+++ src/components/topbarComponent/index.tsx
@@ -1,60 +1,58 @@
 "use client";
-import { Button } from "@/resuableComponents";
-import { Input } from "@/resuableComponents/input";
-import { BellRing, CircleUserRound, Mail, Podcast } from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import image from '../../../public/theme_logo.png'
+import SearchBox from "@/resuableComponents/SearchBox";
+import { Button } from "@/resuableComponents";
+import { CgGirl } from "react-icons/cg";
+import { MdAccessibility } from "react-icons/md";
+import { PiChats } from "react-icons/pi";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { RiCustomerService2Line } from "react-icons/ri";
+import imagesss from '../../../public/Waterfall-landscape.jpg'
+
+
 
 const TopbarComponent = () => {
   return (
-    <div className="container mx-auto px-4 py-1 flex justify-between items-center border-b-2 shadow-lg">
-      <div className="flex">
-        <div className="flex items-center justify-evenly">
-          <a href="#" className="font-bold text-3xl text-cyan-500">
-            connecteam
-          </a>
-          <Input
-            type="text"
-            placeholder="Search anything"
-            className="text-gray-300 border-gray-600 rounded-md p-2 ml-4 w-auto"
-          />
+    <>
+      <div className="py-2 px-4 w-full flex items-center justify-between">
+        <div className="flex gap-2 shrink">
+          <Image src={image} alt="Logo" width={200} height={200} className="object-scale-down" />
+          <SearchBox />
         </div>
-        <div className="flex items-center space-x-4">
-          <div className="flex gap-2 justify-center items-center">
-            <div className="text-gray-500">Free trial ends in</div>
-            <a href="#" className="text-gray-500">
-              5 days
-            </a>
+        <div className="flex gap-4 items-center shrink">
+          <div className=" px-2 border-l-2 flex justify-center">
+            <Button className="rounded-full bg-[#af52de]">Upgrade</Button>
           </div>
-          <Button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-3xl">
-            Upgrade
-          </Button>
-
-          <div className="border-x text-gray-500 flex px-4">
-            <Podcast />
-            <a href="#" className="text-gray-500 px-3">
-              Talk to an expert
-            </a>
-            <a href="#" className="text-gray-500 border-l px-3">
-              Help
-            </a>
+          <div className="text-[#2998ff] flex items-center px-2 border-l-2">
+            <div className="text-2xl"><CgGirl /></div>
+            <div className="whitespace-nowrap">Talk to an Expert</div>
           </div>
-          <div>
-            <Button className="text-gray-500 flex gap-6">
-              <Mail />
-              <BellRing />
-            </Button>
-            <div className="hidden absolute top-full right-0 mt-2  border border-gray-200 rounded-md shadow-md">
-              <div className="p-4"></div>
+          <div className="text-[#2998ff] flex items-center px-2 border-l-2">Help</div>
+          <div className="flex items-center gap-4 text-[#8B939C] px-2 border-l-2">
+            <MdAccessibility className="text-2xl" />
+            <PiChats className="text-2xl" />
+            <RiCustomerService2Line className="text-2xl" />
+            <IoMdNotificationsOutline className="text-2xl" />
+          </div>
+          <div className="flex items-center gap-2 px-2 border-l-2">
+            <div className="relative">
+              <div className="z-0 w-12 h-12 rounded-full">
+                <Image src={imagesss} alt="image" className="w-fill rounded-full h-full "/>
+              </div>
+              <div className="z-40 absolute h-6 -bottom-1 right-0 w-6 border-[3px] border-white rounded-full">
+                <Image src={imagesss} alt="image" className="w-full h-full rounded-full"/>
+              </div>
+            </div>
+            <div className="text-[#2998ff]">
+              <p>Pardeep Kumar</p>
             </div>
           </div>
-          <Button className="text-gray-500 flex gap-2 border-l px-4">
-            <CircleUserRound />
-            <span className="">Pardeep</span>
-          </Button>
         </div>
       </div>
-    </div>
+    </>
   );
-};
+};  
 
 export default TopbarComponent;
