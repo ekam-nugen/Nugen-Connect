2dc9fcd0bf16e8923a4d1c000c72676b3da948cf
Pardeep24k <pardeep1209k@gmail.com>
2024-11-19 13:06:14 +0530

    layout setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import TopbarComponent from "@/components/topbarComponent";
 import "./globals.css";
+import SidebarComponent from "@/components/sidebarComponent";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="h-screen">
+        <div className="shadow-md w-max 1200:w-full sticky z-50 top-0 bg-white h-[9%]">
+          <TopbarComponent />
+        </div>
+        <div className="z-10 flex gap-4 h-[91%] overflow-auto">
+          <div className="overflow-y-auto h-full">
+            <SidebarComponent label="" icon=""/>
+          </div>
+          <div className="w-full overflow-y-auto h-full">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
