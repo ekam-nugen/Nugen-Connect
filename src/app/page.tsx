39e33bf437188ae03fbf1d29a758df0de5f3cda5
Pardeep24k <pardeep1209k@gmail.com>
2024-11-19 13:06:14 +0530

    layout setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,9 @@ import TopbarComponent from "@/components/topbarComponent";
 export default function Home() {
   return (
     <>
-      <div className="w-max shadow-md 1200:w-screen">
-        <TopbarComponent />
+      <div className="">
+        <div className="">Hii</div>
       </div>
-
-
-      {/* w-screen border-2 border-red-600 shadow-md */}
-      {/* <div className="flex absolute top-[12%]">
-        <div className="h-screen">
-          <SidebarComponent icon={""} label={""} />
-        </div>
-        <div className="flex-grow h-screen ml-4 w-screen bg-gray-200 rounded-md">
-          hello
-        </div>
-      </div> */}
     </>
   );
 }
--- src/components/sidebarComponent/index.tsx
+++ src/components/sidebarComponent/index.tsx
@@ -11,24 +11,22 @@ import {
 } from "lucide-react";
 import { OverviewItems, userItems, sidebarItems } from "@/json/sidebar/inedx";
 
-export default function SidebarComponent({}: sidebarItemType) {
+export default function SidebarComponent({ }: sidebarItemType) {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const toggleSidebar = () => setIsCollapsed(!isCollapsed);
 
   return (
     <div
-      className={`relative sidebar ${
-        isCollapsed ? "w-16" : "w-48"
-      } mt-2 min-h-screen top-0 border-r transition-all 
+      className={`overflow-y-auto sidebar border-4 border-red-700${isCollapsed ? "w-16" : "w-48"
+        } mt-2  top-0 border-r transition-all 
       duration-100 ease-in-out`}
     >
       <div
-        className={`flex px-2 bg-white ${
-          isCollapsed
+        className={`flex px-2 bg-white ${isCollapsed
             ? "justify-center items-center pb-3 text-center"
             : "justify-end"
-        }`}
+          }`}
       >
         <button
           onClick={toggleSidebar}
@@ -41,9 +39,8 @@ export default function SidebarComponent({}: sidebarItemType) {
         {OverviewItems.map((item: any, index: number) => (
           <div
             key={index}
-            className={`flex items-center mb-2 h-9 gap-2 rounded-md px-1 group hover:bg-gray-100 shadow-x-lg hover:cursor-pointer ${
-              isCollapsed ? "justify-center" : ""
-            }`}
+            className={`flex items-center mb-2 h-9 gap-2 rounded-md px-1 group hover:bg-gray-100 shadow-x-lg hover:cursor-pointer ${isCollapsed ? "justify-center" : ""
+              }`}
           >
             <span className="text-xl h-7.5 w-7.5 text-zinc-600 flex items-center justify-center rounded-md px-1">
               <item.icon />
@@ -68,9 +65,8 @@ export default function SidebarComponent({}: sidebarItemType) {
         {userItems?.map((item: any, index: number) => (
           <div
             key={index}
-            className={`flex items-center mb-2 h-9 gap-2 rounded-md px-1 hover:bg-gray-100 group hover:cursor-pointer ${
-              isCollapsed ? "justify-center" : ""
-            }`}
+            className={`flex items-center mb-2 h-9 gap-2 rounded-md px-1 hover:bg-gray-100 group hover:cursor-pointer ${isCollapsed ? "justify-center" : ""
+              }`}
           >
             <span className="text-xl h-7.5 w-7.5 text-zinc-600 flex items-center justify-center rounded-md px-1 ">
               <item.icon />
@@ -95,18 +91,64 @@ export default function SidebarComponent({}: sidebarItemType) {
         {sidebarItems?.map((item: any, index: number) => (
           <div
             key={index}
-            className={`flex items-center mb-0.5 h-10 gap-2 rounded-md px-1 hover:bg-gray-100 transition-all group relative hover:cursor-pointer ${
-              isCollapsed
+            className={`flex items-center mb-0.5 h-10 gap-2 rounded-md px-1 hover:bg-gray-100 transition-all group relative hover:cursor-pointer ${isCollapsed
                 ? "justify-center gap-2"
                 : "duration-100 hover:translate-x-2 transition-transform"
-            }`}
+              }`}
           >
             <div
-              className={`${
-                isCollapsed
+              className={`${isCollapsed
                   ? "hidden"
                   : "absolute left-0 flex opacity-0 group-hover:opacity-90"
-              } transition-opacity duration-300`}
+                } transition-opacity duration-300`}
+            >
+              <Grip className="h-4 w-4 text-gray-500" />
+            </div>
+
+            <span
+              className={`text-xl h-6 w-6 text-white flex items-center justify-center rounded-md px-1 group-hover:ml-4  ${item.color}`}
+            >
+              <item.icon />
+            </span>
+            {!isCollapsed && (
+              <p className="ml-1 font-normal text-normal text-gray-700 group-hover:truncate max-w-[120px] text-nowrap">
+                {item?.label}
+              </p>
+            )}
+            {isCollapsed && (
+              <span className="absolute left-12 h-10 py-1 items-center flex justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-gray-100 shadow-lg text-gray-700 text-nowrap text-sm rounded-md px-2 py-1 pointer-events-none">
+                {item?.label}
+              </span>
+            )}
+            {!isCollapsed && (
+              <div className="ml-auto flex justify-end">
+                <span className="group-hover:opacity-90 opacity-0 transition-opacity duration-300">
+                  <EllipsisVertical className="h-4 w-4 text-gray-500" />
+                </span>
+              </div>
+            )}
+          </div>
+        ))}
+
+
+
+
+
+
+
+        {sidebarItems?.map((item: any, index: number) => (
+          <div
+            key={index}
+            className={`flex items-center mb-0.5 h-10 gap-2 rounded-md px-1 hover:bg-gray-100 transition-all group relative hover:cursor-pointer ${isCollapsed
+                ? "justify-center gap-2"
+                : "duration-100 hover:translate-x-2 transition-transform"
+              }`}
+          >
+            <div
+              className={`${isCollapsed
+                  ? "hidden"
+                  : "absolute left-0 flex opacity-0 group-hover:opacity-90"
+                } transition-opacity duration-300`}
             >
               <Grip className="h-4 w-4 text-gray-500" />
             </div>
@@ -137,9 +179,8 @@ export default function SidebarComponent({}: sidebarItemType) {
         ))}
 
         <div
-          className={`flex items-center mb-auto px-2 h-10 mt-2 hover:text-blue-500 gap-2 rounded-xl ${
-            isCollapsed ? "justify-center" : ""
-          }`}
+          className={`flex items-center mb-auto px-2 h-10 mt-2 hover:text-blue-500 gap-2 rounded-xl ${isCollapsed ? "justify-center" : ""
+            }`}
         >
           <span className="text-zinc-900 rounded-full text-center">
             <CirclePlus className="text-blue-500 h-4 w-4 hover:text-gray-400" />
@@ -148,17 +189,16 @@ export default function SidebarComponent({}: sidebarItemType) {
         </div>
       </div>
       <div
-        className={`fixed bottom-0 left-0 w-48 border-1 border border-zinc-200 bg-white ${
-          isCollapsed ? "w-16 hidden" : "w-48"
-        }`}
+        className={`fixed bottom-0 left-0 w-48 border-1 border border-zinc-200 bg-white ${isCollapsed ? "w-16 hidden" : "w-48"
+          }`}
       >
         <hr />
-        <div className="mt-auto px-4 gap-4 flex items-center h-10 mt-2 hover:text-blue-500 gap-2 rounded-xl cursor-pointer">
-          <span className="text-white bg-blue-500 rounded-full">
-            <CirclePlus className="h-4 w-4" />
-          </span>
-          <p>{ADD_SECTION}</p>
-        </div>
+          <div className="mt-auto px-4 gap-4 flex items-center h-10 mt-2 hover:text-blue-500 gap-2 rounded-xl cursor-pointer">
+            <span className="text-white bg-blue-500 rounded-full">
+              <CirclePlus className="h-4 w-4" />
+            </span>
+            <p>{ADD_SECTION}</p>
+          </div>
       </div>
     </div>
   );
