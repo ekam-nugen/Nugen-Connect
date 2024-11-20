858465fc0a1f8920f172e1e094be3896f4699a8e
Pardeep24k <pardeep1209k@gmail.com>
2024-11-20 13:04:11 +0530

    Side bar tooltip fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import DashboardComponent from "@/components/overviewPage";
-import SidebarComponent from "@/components/sidebarComponent";
-import TopbarComponent from "@/components/topbarComponent";
 
 export default function Home() {
   return (
