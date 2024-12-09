cfac8a8fe52369b50ca16a495169373cd10fe00f
Ajaynugen <ajayk.nugen@gmail.com>
2024-12-09 15:25:13 +0530

    create update page

---
+++ src/app/(layout)/update/page.tsx
@@ -0,0 +1,12 @@
+import UpdateComponent from "@/components/updateComponent";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <UpdateComponent />
+    </div>
+  );
+}
+
+export default page;
