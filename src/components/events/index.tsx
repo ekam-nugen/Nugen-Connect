584eb6e15c4fe9abb08e05af5961194f4995f856
Harsimran0429 <156643361+Harsimran0429@users.noreply.github.com>
2025-05-13 11:26:56 +0530

    fix: stepper footer & video player files

--- src/components/events/index.tsx
+++ src/components/events/index.tsx
@@ -1,5 +1,5 @@
 import CardWithImage from "@/components/cardWithImage";
-import VideoPlayer from "@/resuableComponents/videoPlayer-temp";
+import VideoPlayer from "@/resuableComponents/videoPlayer";
 import { Button } from "@/resuableComponents";
 import React from "react";
 import image from "../../../public/Waterfall-landscape.jpg";
